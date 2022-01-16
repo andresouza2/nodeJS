6fa25aebebba0ec511c7ef04205608b4a115b262
Andre <andssouzza@gmail.com>
2022-01-15 20:42:08 -0400

    enviando atualizações de aula do curso de nodeJS

--- index.js
+++ index.js
@@ -1,6 +1,43 @@
 import chalk from 'chalk'
+import fs from 'fs'
 
-console.log(chalk.blue('vamos começar!'));
+function tratarErro(erro) {
+  throw new Error(chalk.red(erro.code, 'Não há um arquivo no caminho especificado'));
+}
+
+async function pegarArquivo(caminhoDoArquivo) {
+  try {
+    const encoding = 'utf-8';
+  const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
+  console.log(chalk.green(texto));
+  } catch(erro) {
+    tratarErro(erro);
+  } finally {
+    console.log(chalk.yellow('operação concluída'));
+  }
+}
+
+/* function pegarArquivo(caminhoDoArquivo) {
+  const encoding = 'utf-8';
+  fs.promises
+  .readFile(caminhoDoArquivo, encoding)
+  .then((texto) => console.log(chalk.green(texto)))
+  .catch((erro) => tratarErro(erro))
+} */
+
+/* function pegarArquivo(caminhoDoArquivo) {
+  const encoding = 'utf-8'
+  fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
+    if (erro) {
+      tratarErro(erro);
+    }
+    console.log(chalk.green(texto));
+  })
+} */
+
+pegarArquivo('./arquivos/texto1.md');
+
+/* console.log(chalk.blue('vamos começar!'));
 
 const paragrafo = 'Texto retornado por uma função'
 
@@ -9,3 +46,12 @@ function texto(string) {
 }
 
 console.log(texto(paragrafo));
+console.log(chalk.blue.bgWhite.bold('Alura'));
+console.log(chalk.blue('curso', 'de', 'NodeJS'));
+console.log(chalk.red('vermelho',
+ chalk.underline.bgBlue('azul')));
+ console.log(`
+CPU: ${chalk.red('90%')}
+RAM: ${chalk.green('40%')}
+DISK: ${chalk.yellow('70%')}
+`); */
---
+++ promessa.js
@@ -0,0 +1,18 @@
+import chalk from 'chalk'
+
+function promessa(bool) {
+  const x = bool;
+  return new Promise((resolve, reject) => {
+    if (!x) {
+      reject(new Error('Falha na promessa'));
+    }
+    resolve('sucesso na promessa')
+  });
+}
+
+function exibeResposta(textoResult) {
+  console.log(chalk.green(textoResult));
+}
+
+promessa(true)
+.then((texto) => exibeResposta(texto));
